6f8567fb8c4f72aa6499d7aec3c3973607767454
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-02-08 13:11:39 +0100

    Cleaned up

--- features/steps/vat.php
+++ features/steps/vat.php
@@ -1,14 +1,14 @@
 <?php
 
-$steps->Given('/^I have entered (\d+) into the unit price$/', function($world, $arg1) {
-    $world->price->setUnitPrice($arg1);
+$steps->Given('/^I have entered (\d+) into the unit price$/', function($world, $unitPrice) {
+    $world->price->setUnitPrice($unitPrice);
 });
-$steps->Given('/^I have entered (\d+) into the vat percentage$/', function($world, $arg1) {
-    $world->price->setVatPercentage($arg1);
+$steps->Given('/^I have entered (\d+) into the vat percentage$/', function($world, $vatPercentage) {
+    $world->price->setVatPercentage($vatPercentage);
 });
-$steps->Given('/^I make a behavior request$/', function($world) {
+$steps->When('/^I make a behavior request$/', function($world) {
     $world->price->modifiedVat();
 });
-$steps->Given('/^The gross price should be (\d+)$/', function($world, $arg1) {
-    assertEquals($arg1, $world->price->getGrossPrice());
+$steps->Then('/^The gross price should be (\d+)$/', function($world, $expectedGrossPrice) {
+    assertEquals($expectedGrossPrice, $world->price->getGrossPrice());
 });
--- features/support/env.php
+++ features/support/env.php
@@ -1,6 +1,10 @@
 <?php
-//require_once...
+// here you could put require_once() statements, or include some bootstrap file
 
+/**
+ * I define a simple assertion function here. However, it should be in another file.
+ * You can also import PHPUnit's assertions: everything that throws exceptions when the assertion fails will work.
+ */
 function assertEquals($expected, $actual)
 {
     if ($expected != $actual) {
@@ -8,14 +12,34 @@ function assertEquals($expected, $actual)
     }
 }
 
+/**
+ * Also the class definition should be in its own file: this file will
+ * be repeatedly executed.
+ */
 class Price
 {
-    function setUnitPrice($price) { $this->unit_price = $price; }
-    function setVatPercentage($vat_percentage) { $this->vat_percentage = $vat_percentage; }
-    function modifiedVat() {
+    function setUnitPrice($price)
+    {
+        $this->unit_price = $price;
+    }
+
+    function setVatPercentage($vat_percentage)
+    {
+        $this->vat_percentage = $vat_percentage;
+    }
+
+    function modifiedVat()
+    {
         $this->gross_price = $this->unit_price * (1 + $this->vat_percentage / 100);
     }
-    function getGrossPrice() { return $this->gross_price; }
+
+    function getGrossPrice()
+    {
+        return $this->gross_price;
+    }
 }
 
+/**
+ * The fixture for our tests.
+ */
 $world->price = new Price();
